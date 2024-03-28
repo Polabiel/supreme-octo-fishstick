07539bbc93460bbd09645783c4cfebb685d744b2
Gabriel Oliveira <bielgabrieloliveira77@gmail.com>
2024-03-28 00:51:28 +0000

    alter: name variable

--- generateMatrix.py
+++ generateMatrix.py
@@ -1,11 +1,11 @@
 import random
 
 def generate_distance_matrix(namesCities: string) -> list[list[int]]:
-    num_cities = len(namesCities)
-    distance_matrix = [[0] * num_cities for _ in range(num_cities)]
+    numCities = len(namesCities)
+    distance_matrix = [[0] * numCities for _ in range(numCities)]
 
-    for i in range(num_cities):
-        for j in range(i + 1, num_cities):
+    for i in range(numCities):
+        for j in range(i + 1, numCities):
             distance = random.randint(1, 100)
             distance_matrix[i][j] = distance
             distance_matrix[j][i] = distance
