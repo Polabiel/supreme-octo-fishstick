5116e548557d87d6f86e1c4850b437e416e1c036
Gabriel Oliveira <40695127+Polabiel@users.noreply.github.com>
2024-03-31 15:01:46 -0300

    feat: Implementation of total distance calculation for the Traveling Salesman algorithm

--- main.py
+++ main.py
@@ -13,11 +13,10 @@ def benchmark(tour: list, numberCities: int, distances: list[list[int]]) -> int:
     return total_distance
 
 if __name__ == "__main__":
-    
     m  = Matriz(["São Paulo","Campinas","Sorocaba","Santos","Ribeirão Preto","Bauru","São José dos Campos","São José do Rio Preto","Piracicaba","Presidente Prudente","Araraquara","Maranhão"])
-    
     distances: list[list[int]] = m.generateRandomMatrix()
     m.toString()
     print("\nResult:\n")
     t = TravelingSalesman(distances)
     print(t.nearest_neighbor())
+    print(f"Total distance calculation: {benchmark(t.nearest_neighbor(), len(t.cities),t.cities)}")
